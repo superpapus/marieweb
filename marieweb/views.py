4426ae2ef39f893f25a2a2291ba88864ea8a606c
PabloRuizRa <pablo.ruizra@usm.cl>
2024-10-21 17:39:00 -0300

    Hecha query para busqueda en otra branch

--- marieweb/views.py
+++ marieweb/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio/inicio.html')
+
+def buscar_productos(request):
+    query = request.GET.get('q', '')  # Obtener el término de búsqueda desde la URL
+    productos = []  # Inicializar con una lista vacía
+
+    if query:
+        productos = Producto.objects.filter(nombre__icontains=query)  # Buscar productos que coincidan
+
+    return render(request, 'buscar.html', {'productos': productos, 'query': query})
