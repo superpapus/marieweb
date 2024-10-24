ddcab3a74a9764a341022bcb39d877ecf417b5ef
Misael Tapia <misaeltapiac71@gmail.com>
2024-10-23 23:17:58 -0300

    Revert "👁️Mejora en la BD para gestionar la visibilidad de productos y categorías"
    This reverts commit c7541530f6ac3e416e8a769120aeb058c8e271f8.

--- marieweb/models.py
+++ marieweb/models.py
@@ -4,7 +4,6 @@ class Categoria(models.Model):
     nombre = models.CharField(max_length=100)  
     descripcion = models.TextField(blank=True, null=True)  
     icon_image = models.ImageField(upload_to='icons/', blank=True, null=True) 
-    enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
@@ -15,8 +14,7 @@ class Producto(models.Model):
     descripcion = models.TextField(blank=True, null=True)  
     precio = models.IntegerField()  
     stock = models.PositiveIntegerField()  
-    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
-    enabled = models.BooleanField(default=True) 
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)  
 
     def __str__(self):
         return self.nombre
