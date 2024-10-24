154778ca0e5b46c92d5ef931c7d2b06bc48d4d46
AshleyIB <ashley.iturriaga@usm.cl>
2024-10-24 17:28:22 -0300

    Front responsivo + estetico

--- marieweb/views.py
+++ marieweb/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Producto
-
+from .models import Producto, Categoria
 
 # Create your views here.
 def inicio(request):
     ordenar = request.GET.get('ordenar', 'destacado')
+    categoria_id = request.GET.get('categoria', None)
     
     if ordenar == 'menor-precio':
         productos = Producto.objects.all().order_by('precio')
@@ -12,7 +12,24 @@ def inicio(request):
         productos = Producto.objects.all().order_by('-precio')
     else:
         productos = Producto.objects.all()  # Default order uwu
+    
+    categorias = Categoria.objects.all()
 
+    if categoria_id:
+        productos = productos.filter(categoria_id=categoria_id)
 
+    return render(request, 'inicio/inicio.html', {
+        'productos': productos, 
+        'ordenar': ordenar, 
+        'categorias': categorias, 
+        'categoria_actual': categoria_id
+        })
 
-    return render(request, 'inicio/inicio.html', {'productos': productos, 'ordenar': ordenar})
+def buscar_productos(request):
+    query = request.GET.get('q', '')  # Obtener el término de búsqueda desde la URL
+    productos = []  # Inicializar con una lista vacía
+
+    if query:
+        productos = Producto.objects.filter(nombre__icontains=query)  # Buscar productos que coincidan
+
+    return render(request, 'buscar.html', {'productos': productos, 'query': query})
