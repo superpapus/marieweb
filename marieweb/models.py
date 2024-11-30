820e4712a93c2d0c221ca76c7a37bfe24a798fc8
Misael Tapia <misaeltapiac71@gmail.com>
2024-11-30 13:58:15 -0300

    :construction: #41 Agregado modelo Deuda

--- marieweb/models.py
+++ marieweb/models.py
@@ -1,22 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)  
-    descripcion = models.TextField(blank=True, null=True)  
-    icon_image = models.ImageField(upload_to='icons/', blank=True, null=True) 
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    icon_image = models.ImageField(upload_to='icons/', blank=True, null=True)
     enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
     
 class Producto(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  
-    nombre = models.CharField(max_length=100)  
-    descripcion = models.TextField(blank=True, null=True)  
-    precio = models.IntegerField()  
-    stock = models.PositiveIntegerField()  
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.IntegerField()
+    stock = models.PositiveIntegerField()
     imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
-    enabled = models.BooleanField(default=True) 
+    enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
+
+class Deuda(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=False, null=False, related_name='deudas')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    precioProducto = models.IntegerField(default=0)
+    cantidad = models.PositiveIntegerField(default=1)
+    fecha = models.DateTimeField(auto_now_add=True)
+    pagado = models.BooleanField(default=False)
+    fechaPago = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.usuario.username + ' - ' + self.producto.nombre
--- marieweb/views.py
+++ marieweb/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Producto, Categoria
+from .models import Producto, Categoria, Deuda
 
 # Create your views here.
 def inicio(request):
